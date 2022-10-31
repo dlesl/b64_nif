b91658e2a77c60b1f68c8d87f55ce9483060fc13
David <dlesl@users.noreply.github.com>
2022-10-31 22:40:27 +0100

    add specs

--- src/b64_nif.erl
+++ src/b64_nif.erl
@@ -3,9 +3,11 @@
 -export([encode/1, decode/1]).
 -on_load(init/0).
 
+-spec encode(iodata()) -> binary().
 encode(_Data) ->
   erlang:nif_error(nif_not_loaded).
 
+-spec decode(iodata()) -> binary().
 decode(_Data) ->
   erlang:nif_error(nif_not_loaded).
 
